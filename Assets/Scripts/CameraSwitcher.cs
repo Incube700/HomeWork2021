9101e5ce34dda02ddd3e6942b086cf812108a95d
Incube700 <Belkajor@gmail.com>
2025-11-23 11:56:07 +0530

    Камера!!

--- Assets/Scripts/CameraSwitcher.cs
+++ Assets/Scripts/CameraSwitcher.cs
@@ -1,18 +1,69 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
-public class CameraSwitcher : MonoBehaviour
+
+public class CameraSwitcherCinemachine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private CinemachineVirtualCamera[] _virtualCameras;
+
+    [SerializeField] private int _startIndex = 0;
+
+    private int _currentIndex;
+
+    private void Start()
     {
-        
+        SetActiveCamera(_startIndex);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SetActiveCamera(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SetActiveCamera(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SetActiveCamera(2);
+        }
+    }
+
+    private void SetActiveCamera(int index)
+    {
+        if (_virtualCameras == null)
+        {
+            return;
+        }
+
+        if (index < 0 || index >= _virtualCameras.Length)
+        {
+            return;
+        }
+
+        _currentIndex = index;
+
+        for (int i = 0; i < _virtualCameras.Length; i++)
+        {
+            CinemachineVirtualCamera virtualCamera = _virtualCameras[i];
+
+            if (virtualCamera == null)
+            {
+                continue;
+            }
+
+            if (i == _currentIndex)
+            {
+                virtualCamera.Priority = 20;
+            }
+            else
+            {
+                virtualCamera.Priority = 0;
+            }
+        }
     }
 }
